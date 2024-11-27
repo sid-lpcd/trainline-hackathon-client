ee3a2a0b8fdc28b907f4ce036b901ac0a5fa75db
Sid <sidoniomiguelsilva567@gmail.com>
2024-11-27 16:33:24 +0000

    Add mobile layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,23 @@
 import "./Header.scss";
 import TrainlineMainLogo from "../../assets/logo/TrainlineMain.svg?react";
-import BasketIcon from "../../assets/logo/basket-icon.svg?react";
-import MenuIcon from "../../assets/logo/menu-icon.svg?react";
-import ProfileIcon from "../../assets/logo/profile-icon.svg?react";
+import BasketIcon from "../../assets/icons/basket-icon.svg?react";
+import MenuIcon from "../../assets/icons/menu-icon.svg?react";
+import ProfileIcon from "../../assets/icons/profile-icon.svg?react";
+
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="header--wrapper">
-        <Link to="/">
-          <TrainlineMainLogo />
-        </Link>
-        <div className="header__mobile">
-          <BasketIcon />
-          <MenuIcon />
-          <ProfileIcon />
-        </div>
-        <div className="header__desktop"></div>
+    <header className="header header--wrapper">
+      <Link to="/">
+        <TrainlineMainLogo />
+      </Link>
+      <div className="header__mobile">
+        <BasketIcon className="header__basket-icon" />
+        <MenuIcon className="header__menu-icon" />
+        <ProfileIcon className="header__profiles-icon" />
       </div>
+      <div className="header__desktop"></div>
     </header>
   );
 };
