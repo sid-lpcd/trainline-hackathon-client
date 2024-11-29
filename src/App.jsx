5c9d4d8e51f71e82520571b377a77ce5c244391e
Sid <sidoniomiguelsilva567@gmail.com>
2024-11-29 12:14:26 +0000

    Fix git errors

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,7 @@ function App() {
           <Route path="/selection" element={<SelectionPage />} />
           <Route path="/payment/:location?" element={<PaymentPage />} />
           <Route path="/booking" element={<BookingConfirmation />} />
-<<<<<<< HEAD
           <Route path="/tickets" element={<TicketPage />} />
-
-=======
-          <Route path="/ticket" element={<TicketPage />} />
->>>>>>> develop
           <Route path="/explore" element={<ExplorePage />} />
         </Routes>
       </div>
