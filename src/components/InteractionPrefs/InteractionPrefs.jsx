a478768cf76752dceef75727887291c7b58e346f
Gurpreet Chana <gurpreetchana_@outlook.com>
2024-11-29 12:10:56 +0000

    update interaction and payment pages

--- src/components/InteractionPrefs/InteractionPrefs.jsx
+++ src/components/InteractionPrefs/InteractionPrefs.jsx
@@ -1,30 +1,107 @@
-import "InteractionPrefs.scss";
+import "./InteractionPrefs.scss";
 import checkbox from "../../assets/icons/checkbox.svg";
 import checkboxFilled from "../../assets/icons/checkbox-checked.svg";
+import { useState } from "react";
 
-function InteractionPrefs() {
+function InteractionPrefs({ setInteractionsVisible }) {
+  const [selectedStates, setSelectedStates] = useState([false, false]);
+
+  const toggleSelection = (index) => {
+    setSelectedStates((prevStates) => {
+      const updatedStates = [...prevStates];
+      updatedStates[index] = !updatedStates[index];
+      return updatedStates;
+    });
+  };
   return (
     <>
       <header className="header">
+        <div className="spacer"></div>
         <h2 className="header__title">Interaction preferences</h2>
-        <p className="header__close">Done</p>
+        <p
+          className="header__close"
+          onClick={() => {
+            setInteractionsVisible(false);
+          }}
+        >
+          Done
+        </p>
       </header>
       <main className="list">
         <article className="list__item">
           <p className="list__item-text">No Preference</p>
           <div
-            onClick={() => {
-              toggleLike();
-            }}
+            className="list__item-checkbox"
+            onClick={() => toggleSelection(0)}
           >
-            {liked ? (
-              <HeartIcon className="card-explore__icon card-explore__icon--liked" />
+            {selectedStates[0] ? (
+              <img src={checkboxFilled} alt="Checkbox filled" />
             ) : (
-              <HeartIcon className="card-explore__icon" />
+              <img src={checkbox} alt="Checkbox empty" />
+            )}
+          </div>
+        </article>
+        <article className="list__item">
+          <p className="list__item-text">Having a conversation</p>
+          <div
+            className="list__item-checkbox"
+            onClick={() => toggleSelection(1)}
+          >
+            {selectedStates[1] ? (
+              <img src={checkboxFilled} alt="Checkbox filled" />
+            ) : (
+              <img src={checkbox} alt="Checkbox empty" />
+            )}
+          </div>
+        </article>
+        <article className="list__item">
+          <p className="list__item-text">Playing card games</p>
+          <div
+            className="list__item-checkbox"
+            onClick={() => toggleSelection(2)}
+          >
+            {selectedStates[2] ? (
+              <img src={checkboxFilled} alt="Checkbox filled" />
+            ) : (
+              <img src={checkbox} alt="Checkbox empty" />
+            )}
+          </div>
+        </article>
+        <article className="list__item">
+          <p className="list__item-text">Helping the elderly</p>
+          <div
+            className="list__item-checkbox"
+            onClick={() => toggleSelection(3)}
+          >
+            {selectedStates[3] ? (
+              <img src={checkboxFilled} alt="Checkbox filled" />
+            ) : (
+              <img src={checkbox} alt="Checkbox empty" />
+            )}
+          </div>
+        </article>
+        <article className="list__item">
+          <p className="list__item-text">Food 4 Thoughts</p>
+          <div
+            className="list__item-checkbox"
+            onClick={() => toggleSelection(4)}
+          >
+            {selectedStates[4] ? (
+              <img src={checkboxFilled} alt="Checkbox filled" />
+            ) : (
+              <img src={checkbox} alt="Checkbox empty" />
             )}
           </div>
         </article>
       </main>
+      <button
+        className="submit"
+        onClick={() => {
+          setInteractionsVisible(false);
+        }}
+      >
+        Done
+      </button>
     </>
   );
 }
--- src/pages/PaymentPage/PaymentPage.jsx
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -4,9 +4,18 @@ import paymentTicket from "../../assets/images/PaymentScreen/paymentTicket.jpeg"
 import paymentOptions from "../../assets/images/PaymentScreen/paymentOptions.jpeg";
 import paymentAdvert from "../../assets/images/PaymentScreen/paymentAdvert.jpeg";
 import paymentCard from "../../assets/images/PaymentScreen/paymentCard.jpeg";
+import friendsicon from "../../assets/icons/friends-icon.svg";
+import addIcon from "../../assets/icons/add-icon.svg";
 import { Link } from "react-router-dom";
+import InteractionPrefs from "../../components/InteractionPrefs/InteractionPrefs";
+import { useState } from "react";
 
 function PaymentPage() {
+  const [interactionsVisible, setInteractionsVisible] = useState(false);
+
+  if (interactionsVisible) {
+    return <InteractionPrefs setInteractionsVisible={setInteractionsVisible} />;
+  }
   return (
     <>
       <header className="header">
@@ -27,6 +36,45 @@ function PaymentPage() {
           src={paymentOptions}
           alt="payment page options template"
         />
+        <article
+          className="interactions"
+          onClick={() => {
+            setInteractionsVisible(!interactionsVisible);
+          }}
+        >
+          <p className="interactions__text">
+            I am open to interaction with new people
+          </p>
+          <div className="addIcon">
+            <img src={addIcon} alt="Plus icon" />
+          </div>
+        </article>
+        <article className="friends">
+          <div className="friends__wrapper">
+            <img
+              className="friends__icon"
+              src={friendsicon}
+              alt="friends icon"
+            />
+            <div className="friends__wrapper-form">
+              <p className="friends__title">Travelling with friends</p>
+              <p className="friends__text">
+                Invite your friend to share journey
+              </p>
+              <form className="form">
+                <label className="form__label" name="email">
+                  Email
+                </label>
+                <div className="input__wrapper">
+                  <input className="form__input" name="email" type="email" />
+                  <button className="form__add" type="submit">
+                    <img src={addIcon} alt="icon to add" />
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </article>
         <img
           className="header__payment"
           src={paymentAdvert}
