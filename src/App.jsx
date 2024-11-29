857212bfd7635bd715e5ca61e3299c36351128ac
Sid <sidoniomiguelsilva567@gmail.com>
2024-11-29 07:04:09 +0000

    Add rewards tab in ticket page

--- src/App.jsx
+++ src/App.jsx
@@ -14,29 +14,23 @@ import SelectionPage from "./pages/SelectionPage/SelectionPage";
 import Navigation from "./components/Navigation/Navigation";
 import ExplorePage from "./pages/ExplorePage/ExplorePage";
 
-
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-
-
-
       <div className="container">
         <Routes>
-                  <Route path="/" element={<SearchPage />} />
+          <Route path="/" element={<SearchPage />} />
           <Route path="/selection" element={<SelectionPage />} />
           <Route path="/payment" element={<PaymentPage />} />
           <Route path="/booking" element={<BookingConfirmation />} />
-          <Route path="/ticket" element={<TicketPage />} />
+          <Route path="/tickets" element={<TicketPage />} />
 
           <Route path="/explore" element={<ExplorePage />} />
-            
         </Routes>
       </div>
       <Navigation />
-
     </>
   );
 }
