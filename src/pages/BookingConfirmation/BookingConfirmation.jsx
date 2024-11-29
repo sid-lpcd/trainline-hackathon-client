2e3d00642f438e31fb079bed021ed0650e05617b
Sid <sidoniomiguelsilva567@gmail.com>
2024-11-29 14:43:12 +0000

    Add timeout to booking page

--- src/pages/BookingConfirmation/BookingConfirmation.jsx
+++ src/pages/BookingConfirmation/BookingConfirmation.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import booking from "../../assets/images/BookingConfirmation.jpg";
 import "./BookingConfirmation.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function BookingConfirmation() {
+  const navigate = useNavigate();
+  const backToHome = async () => {
+    setTimeout(() => {
+      navigate("/");
+    }, 3000);
+  };
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    backToHome();
+  }, []);
+
   return (
     <>
       <main>
